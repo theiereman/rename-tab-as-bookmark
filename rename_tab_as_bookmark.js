1bcaff612b5dfe1f4a91d81d71aea6b28559f8ef
Thomas Eyermann <thomas.eyermann@protonmail.com>
2024-04-21 19:10:53 +0200

    basic extension version

---
+++ rename_tab_as_bookmark.js
@@ -0,0 +1,57 @@
+async function requestPermissionsIfNeeded() {
+  let neededPermissions = {
+    origins: ["<all_urls>"],
+    permissions: ["tabs", "bookmarks", "scripting"],
+  };
+
+  console.log("requestPermissionsIfNeeded");
+
+  if (!(await browser.permissions.contains(neededPermissions))) {
+    await browser.permissions.request(neededPermissions);
+  }
+}
+
+async function renameAllBookmarkedTabs() {
+  try {
+    let tabs = await browser.tabs.query({});
+    tabs.forEach(async (tab) => {
+      try {
+        //search a bookmark corresponding to tab url
+        let foundBookmarks = await browser.bookmarks.search({
+          url: tab.url,
+        });
+
+        //no bookmark found for tab url
+        if (foundBookmarks.length === 0) {
+          return;
+        }
+
+        //script that changes document title
+        await browser.scripting.executeScript({
+          target: {
+            tabId: tab.id,
+          },
+          func: (tabTitle) => {
+            document.title = tabTitle;
+          },
+          args: [foundBookmarks[0].title],
+        });
+      } catch (e) {
+        console.log(`Error for the tab ${tab.title} (${tab.url})`, e);
+        return;
+      }
+    });
+  } catch (e) {
+    console.log(e);
+    return;
+  }
+}
+
+//fire event at each new tab
+browser.tabs.onUpdated.addListener(requestPermissionsIfNeeded);
+browser.tabs.onUpdated.addListener(renameAllBookmarkedTabs);
+
+//checks for bookmark changes (new bookmark, deletion, name change, url change)
+browser.bookmarks.onCreated.addListener(renameAllBookmarkedTabs);
+browser.bookmarks.onRemoved.addListener(renameAllBookmarkedTabs);
+browser.bookmarks.onChanged.addListener(renameAllBookmarkedTabs);
